5856245273edb6ea3659edb8f0f71815cd6a0dd1
aldyHelix <aldy.satria07@gmail.com>
2022-07-25 09:27:59 +0700

    initial commit

---
+++ application/views/frontend/index.php
@@ -0,0 +1,43 @@
+	<div class="owl-carousel owl-loaded owl-nav-dots-inner" data-options='{"items":1,"loop":true}'>
+		<div class="owl-item">
+			<div class="slider-item" style="background-image:url(<?=base_url()?>assets/frontend/img/sipesan/spandukk.jpg); height: 600px">
+				<div class="container">
+					<div class="slider-item-inner">
+						<div class="slider-item-caption-left slider-item-caption-white">
+							</div>
+						</div>
+				</div>
+			</div>
+		</div>
+		<div class="owl-item">
+			<div class="slider-item" style="background-image:url(<?=base_url()?>assets/frontend/img/sipesan/kartu_namaa.jpg); height: 600px">
+				<div class="container">
+					<div class="slider-item-inner">
+						<div class="slider-item-caption-left slider-item-caption-white">
+							</div>
+						</div>
+				</div>
+			</div>
+		</div>
+		<div class="owl-item">
+			<div class="slider-item" style="background-image:url(<?=base_url()?>assets/frontend/img/sipesan/katalog_brosurr.jpg); height: 600px">
+				<div class="container">
+					<div class="slider-item-inner">
+						<div class="slider-item-caption-left slider-item-caption-white">
+							</div>
+						</div>
+				</div>
+			</div>
+		</div>
+		<div class="owl-item">
+			<div class="slider-item" style="background-image:url(<?=base_url()?>assets/frontend/img/sipesan/stickerr.jpg); height: 600px">
+				<div class="container">
+					<div class="slider-item-inner">
+						<div class="slider-item-caption-left slider-item-caption-white">
+							</div>
+						</div>
+				</div>
+			</div>
+		</div>
+
+	</div>
---
+++ application/views/frontend/templates/footer.php
@@ -0,0 +1,112 @@
+<div class="gap"></div>
+
+<footer class="main-footer">
+	<div class="container">
+		<div class="row row-col-gap" data-gutter="60">
+			<div class="col-md-3">
+				<h4 class="widget-title-sm">Tentang Kami</h4>
+				<p>CV. Kompas adalah sebuah perusahaan yang bergerak dibidang penerbitan dan percetakan transaksi yang menghasilkan berbagai produk seperti majalah, brosur, kalender, undangan, buku kenangan, label sepatu, stiker produk makanan, kardus kue dan lain-lain</p>
+				<ul class="main-footer-social-list">
+					<li>
+						<a class="fa fa-facebook" href="#"></a>
+					</li>
+					<li>
+						<a class="fa fa-twitter" href="#"></a>
+					</li>
+					<li>
+						<a class="fa fa-pinterest" href="#"></a>
+					</li>
+					<li>
+						<a class="fa fa-instagram" href="#"></a>
+					</li>
+					<li>
+						<a class="fa fa-google-plus" href="#"></a>
+					</li>
+				</ul>
+			</div>
+			<div class="col-md-3">
+				<h4 class="widget-title-sm">Alamat</h4>
+				<p>- Percetakan banjarbaru Jl. A. Yani KM. 32,5 Loktabat, Banjarbaru</p>
+				<p>- Percatakan mtp Jl. A. Yani KM. 39 Komp. P. Antasari No. 38, Martapura </p>
+			</div>
+			<div class="col-md-3">
+				<h4 class="widget-title-sm">Kontak</h4>
+				<p>0851-0035-7239 </p>
+				<p>Repost by <a  title="illma" target="_blank">illma</a></p>
+			</div>
+			<div class="col-md-3">
+				<h4 class="widget-title-sm">Jam Buka</h4>
+				<table>
+					<tr>
+						<td style="padding: 3px;">Senin</td>
+						<td style="padding: 3px;"> : </td>
+						<td style="padding: 3px;">09.00 – 17:30</td>
+					</tr>
+					<tr>
+						<td style="padding: 3px;">Selasa</td>
+						<td style="padding: 3px;"> : </td>
+						<td style="padding: 3px;">09.00 – 17:30</td>
+					</tr>
+					<tr>
+						<td style="padding: 3px;">Rabu</td>
+						<td style="padding: 3px;"> : </td>
+						<td style="padding: 3px;">09.00 – 17:30</td>
+					</tr>
+					<tr>
+						<td style="padding: 3px;">Kamis</td>
+						<td style="padding: 3px;"> : </td>
+						<td style="padding: 3px;">09.00 – 17:30</td>
+					</tr>
+					<tr>
+						<td style="padding: 3px;">Jum'at</td>
+						<td style="padding: 3px;"> : </td>
+						<td style="padding: 3px;">09.00 – 17:30</td>
+					</tr>
+					<tr>
+						<td style="padding: 3px;">Sabtu</td>
+						<td style="padding: 3px;"> : </td>
+						<td style="padding: 3px;">09.00 – 17:30</td>
+					</tr>
+					<p>Tanggal Merah Libur</p>
+				</table>
+			</div>
+		</div>
+
+	</div>
+</footer>
+<div class="copyright-area">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<p class="copyright-text">Copyright &copy; <a href="http://wikidevia.digtive.id/">Digtive Developer</a> 2022. illma</p>
+			</div>
+			<div class="col-md-6">
+			</div>
+		</div>
+	</div>
+</div>
+</div>
+<script src="<?= base_url() ?>assets/frontend/js/jquery.js"></script>
+<script src="<?= base_url() ?>assets/frontend/js/bootstrap.js"></script>
+<script src="<?= base_url() ?>assets/frontend/js/icheck.js"></script>
+<script src="<?= base_url() ?>assets/frontend/js/ionrangeslider.js"></script>
+<script src="<?= base_url() ?>assets/frontend/js/jqzoom.js"></script>
+<script src="<?= base_url() ?>assets/frontend/js/card-payment.js"></script>
+<script src="<?= base_url() ?>assets/frontend/js/owl-carousel.js"></script>
+<script src="<?= base_url() ?>assets/frontend/js/magnific.js"></script>
+<script src="<?= base_url() ?>assets/frontend/js/custom.js"></script>
+
+
+<script src="<?= base_url() ?>assets/frontend/js/switcher.js"></script>
+
+
+<script src="<?= base_url() ?>assets/backend/node_modules/dropify/dist/js/dropify.min.js"></script>
+<script src="<?= base_url() ?>assets/backend/js/dropify.js"></script>
+
+<script src="<?= base_url() ?>assets/frontend/js/sipesan.js"></script>
+
+
+</body>
+
+
+</html>
